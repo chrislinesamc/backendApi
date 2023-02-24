cdb62bb2835e35ef6f0f2c645971e7fccc72e962
Chrisline Sam <chrislinesam@htic.iitm.ac.in>
2023-02-24 19:12:08 +0530

    brain details

--- backendApi.py
+++ backendApi.py
@@ -8,6 +8,8 @@ def getListOfSpecies(userHash):
     brainsListResp = requests.get(f'{constants.backendBase}/CC/getDetailsOfSeriesStatsAllRestricted/{userHash}')
     return brainsListResp.text
 
+def get_brain_list(self):
+
     species_list = []
     brain_list = []
 
@@ -15,7 +17,7 @@ def getListOfSpecies(userHash):
     for i in range(len(brainsList['data'])):
         print(brainsList['data'][i]['data']['species_name'])
     print(species_list)
-    print("List of  Brains : ")
+
     for i in range(len(brainsList['data'][3]['children'][0]['children'])):
         brain_name = brainsList['data'][3]['children'][0]['children'][i]['data']['brain_name']
         series_set_id = brainsList['data'][3]['children'][0]['children'][i]['data']['seriesset_id']
@@ -27,23 +29,20 @@ def getListOfSpecies(userHash):
         print("Single brain details using seriesset id :")
         seriessetDetailsResp = requests.get(f'{constants.backendBase}/GW/getBrainThumbNailDetails/IIT/V1/SS-15:-1:-1')
         seriessetDetail = json.loads(seriessetDetailsResp.text)
-        print(seriessetDetail)
-    def get_seriestype_list(self):
-        print("-----------")
-        print("List Of Seriestype")
+
         for i in range(len(self.seriessetDetail)):
             print(self.seriessetDetail[i]['seriesType'])
-
-    def get_section_no(self):
-        print("-----------")
-        print("List of sectionno")
 
         for i in range(len(self.seriessetDetail[4]['thumbnails'])):
             print(self.seriessetDetail[4]['thumbnails'][i]['brainid'])
 
-        print("-----------")
+        return seriessetDetail
+
+    def get_section_no(self):
+
         sectionDetaisRest = requests.get(f'{constants.backendBase}/GW/getBrainViewerDetails/IIT/V1/SS-15:10:1186').text
         sectionDetails = json.loads(sectionDetaisRest)
+        return sectionDetails
 
     def get_annotation_types(self):
         
@@ -51,7 +50,7 @@ def getListOfSpecies(userHash):
         headers= {'Authorization': 'Basic YWRtaW46YWRtaW4='}
         annotationTypeResp = requests.get(f'{constants.backendBase}/BR/AppAnnotationType/', headers={'Authorization': 'Basic YWRtaW46YWRtaW4='})
         annotationType = json.loads(annotationTypeResp.text)
-        print(annotationType)
+        return annotationType
 
 
 
