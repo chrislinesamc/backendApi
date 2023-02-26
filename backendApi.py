6bd18515470271fe14595f3c2eb45a2ac2812b92
Chrisline Sam C <chrislinesam@gmail.com>
2023-02-26 19:43:53 +0530

    basic functions

--- backendApi.py
+++ backendApi.py
@@ -8,7 +8,7 @@ def getListOfSpecies(userHash):
     brainsListResp = requests.get(f'{constants.backendBase}/CC/getDetailsOfSeriesStatsAllRestricted/{userHash}')
     return brainsListResp.text
 
-def get_brain_list(self):
+def get_brain_list():
 
     species_list = []
     brain_list = []
@@ -25,33 +25,20 @@ def get_brain_list(self):
     print(brain_list)
     return species_list,brain_list 
 
-    def get_single_brain_details(self):
-        print("Single brain details using seriesset id :")
-        seriessetDetailsResp = requests.get(f'{constants.backendBase}/GW/getBrainThumbNailDetails/IIT/V1/SS-15:-1:-1')
-        seriessetDetail = json.loads(seriessetDetailsResp.text)
+def getSingleBrainDetails(seriesSetId):
+    seriessetDetailsResp = requests.get(f'https://apollo2.humanbrain.in/GW/getBrainThumbNailDetails/IIT/V1/SS-{seriesSetId}:-1:-1')
+    seriessetDetail = json.loads(seriessetDetailsResp.text)
+    return seriessetDetail
 
-        for i in range(len(self.seriessetDetail)):
-            print(self.seriessetDetail[i]['seriesType'])
+def getListOfSeriesTypeForBrain(seriesSetId):
+    seriesType = []
+    seriessetDetailsResp = requests.get(f'https://apollo2.humanbrain.in/GW/getBrainThumbNailDetails/IIT/V1/SS-{seriesSetId}:-1:-1')
+    seriessetDetail = json.loads(seriessetDetailsResp.text)
+    for i in range(len(seriessetDetail)):
+        seriesType.append(seriessetDetail[i]['seriesType'])
+    return seriesType
 
-        for i in range(len(self.seriessetDetail[4]['thumbnails'])):
-            print(self.seriessetDetail[4]['thumbnails'][i]['brainid'])
-
-        return seriessetDetail
-
-    def get_section_no(self):
-
-        sectionDetaisRest = requests.get(f'{constants.backendBase}/GW/getBrainViewerDetails/IIT/V1/SS-15:10:1186').text
-        sectionDetails = json.loads(sectionDetaisRest)
-        return sectionDetails
-
-    def get_annotation_types(self):
-        
-
-        headers= {'Authorization': 'Basic YWRtaW46YWRtaW4='}
-        annotationTypeResp = requests.get(f'{constants.backendBase}/BR/AppAnnotationType/', headers={'Authorization': 'Basic YWRtaW46YWRtaW4='})
-        annotationType = json.loads(annotationTypeResp.text)
-        return annotationType
-
-
-
-
+def getSectionDetails(sectionNo):
+    sectionDetaisRest = requests.get(f'https://apollo2.humanbrain.in/GW/getBrainViewerDetails/IIT/V1/SS-15:10:{sectionNo}').text
+    sectionDetails = json.loads(sectionDetaisRest)
+    return sectionDetails['Section'][0]
